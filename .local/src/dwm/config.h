be724d37b067b260f3572c103da64aec66f5fac1
Blue Pho3nix <148140471+blue-pho3nix@users.noreply.github.com>
2025-04-14 12:38:39 -0500

    Update config.h

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -11,19 +11,19 @@ static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 16;       /* snap pixel */
 static unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "NerdFontMono-Regular:size=14", "SymbolsNerdFontMono-Regular:pixelsize=18:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#495e57"; // Exterior BG
-static char normbordercolor[]       = "#201f1e";
-static char normfgcolor[]           = "#a4ccbd";
-static char selfgcolor[]            = "#fbf1c7";
-static char selbordercolor[]        = "#bdae93";
-static char selbgcolor[]            = "#495e57";
+static char normbgcolor[]           = "#0a0a0d"; // Exterior BG
+static char normbordercolor[]       = "#0a0a0d";
+static char normfgcolor[]           = "#f6eff6";
+static char selfgcolor[]            = "#46e6fe";
+static char selbordercolor[]        = "#a2f766";
+static char selbgcolor[]            = "#0a0a0d";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
