bdab8fd4d1a22d4fc4049e5695ce063c1624796a
Blue Pho3nix <148140471+blue-pho3nix@users.noreply.github.com>
2025-04-23 16:34:06 -0500

    Update config.h

--- .local/src/dmenu/config.h
+++ .local/src/dmenu/config.h
@@ -13,8 +13,8 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-        [SchemeNorm] = { "#6260fa", "#4c0a0d" },
-        [SchemeSel] = { "#f61e29", "#4c0a0d" },
+        [SchemeNorm] = { "#fef8f8", "#0f0203" },
+        [SchemeSel] = { "#F51D28", "#0f0203" },
         [SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
