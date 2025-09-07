f9fab87f10438596b9a6484e1d17bed6be68f845
Blue Pho3nix <148140471+blue-pho3nix@users.noreply.github.com>
2025-09-07 11:59:37 -0500

    Update color scheme in dmenu configuration

--- .local/src/dmenu/config.h
+++ .local/src/dmenu/config.h
@@ -12,9 +12,9 @@ static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-        [SchemeNorm] = { "#fef8f8", "#0f0203" },
-        [SchemeSel] = { "#F51D28", "#0f0203" },
+		/*     fg         bg       */
+        [SchemeNorm] = { "#29fffe", "#010083" },
+        [SchemeSel] = { "#fe13fa", "#010083" },
         [SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
@@ -34,4 +34,3 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* #FFFFFF #100009 #eeeeee #300009 #000000 #00ffff*/
-
