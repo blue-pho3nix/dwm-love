fcb73838431ab24ada053568d2e4d91bc7cbc3b0
Blue Pho3nix <148140471+blue-pho3nix@users.noreply.github.com>
2025-09-07 14:50:04 -0500

    Fix path export in dwmblocks.sh

--- .local/bin/dwmblocks.sh
+++ .local/bin/dwmblocks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export PATH="$PATH:/home/blue/.local/bin/statusbar";/usr/bin/setsid /usr/local/bin/dwmblocks & 
+export PATH="$PATH:~/.local/bin/statusbar";/usr/bin/setsid /usr/local/bin/dwmblocks & 
