867da08f3a78150b7c70905bf111cac98a4e7115
Blue Pho3nix <148140471+blue-pho3nix@users.noreply.github.com>
2025-09-07 12:00:42 -0500

    Update color scheme and terminal font in config.h

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -18,12 +18,12 @@ static int smartgaps          = 0;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "NerdFontMono-Regular:size=14", "SymbolsNerdFontMono-Regular:pixelsize=18:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#0f0203"; // Exterior BG
-static char normbordercolor[]       = "#0f0203";
-static char normfgcolor[]           = "#fef8f8";
-static char selfgcolor[]            = "#F51D28";
-static char selbordercolor[]        = "#F51D28";
-static char selbgcolor[]            = "#0f0203";
+static char normbgcolor[]           = "#010083"; // Exterior BG
+static char normbordercolor[]       = "#000000";
+static char normfgcolor[]           = "#29fffe";
+static char selfgcolor[]            = "#fe13fa";
+static char selbordercolor[]        = "#010083";
+static char selbgcolor[]            = "#010083";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
@@ -35,7 +35,7 @@ typedef struct {
 	const void *cmd;
 } Sp;
 const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
-const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-f", "NerdFontMono-Regular:size=14", "-g", "50x20", "-e", "bc", "-lq", NULL };
+const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-f", "monospace:size=14", "-g", "50x20", "-e", "bc", "-lq", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
@@ -354,4 +354,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,		0,		     Button5,	     shiftview,      {.i = 1} },
 	{ ClkRootWin,		0,		     Button2,	     togglebar,      {0} },
 };
-
